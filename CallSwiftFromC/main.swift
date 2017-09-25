0eee45925a5c1303bcdc4c61b20ad03ae25719ce
Sua Le <sua8051@gmail.com>
2017-09-25 11:00:17 +0700

    Initial Commit

---
+++ CallSwiftFromC/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  CallSwiftFromC
+//
+//  Created by Sua Le on 9/25/17.
+//  Copyright © 2017 Sua Le. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
